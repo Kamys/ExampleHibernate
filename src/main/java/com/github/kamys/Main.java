75193fd31e0dd3ae1904bdff226a07a29c4c232e
Hikita Kamyshenko <hnkntoc@gmail.com>
2017-01-28 22:31:23 +0300

    Added save Client in base.

--- src/main/java/com/github/kamys/Main.java
+++ src/main/java/com/github/kamys/Main.java
@@ -3,21 +3,59 @@ package com.github.kamys;
 import com.github.kamys.entity.Client;
 import com.github.kamys.entity.Product;
 import com.github.kamys.entity.Store;
+import org.apache.log4j.Logger;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
 
+import java.io.Serializable;
 import java.util.HashMap;
 
 /**
  * This main.
  */
 public class Main {
+    private static final Logger LOGGER = Logger.getLogger(Main.class);
+    private static final Client steve = new Client("Steve", 300);
+
     public static void main(String[] args) {
+        SessionFactory factory = createSessionFactory();
+        Transaction tr = null;
+        try (
+                Session session = factory.openSession()
+        ) {
+            tr = session.beginTransaction();
+            Serializable save = session.save(steve);
+            LOGGER.debug("Serializable = " + save);
+            tr.commit();
+        } catch (HibernateException e) {
+            if (tr != null) tr.rollback();
+            LOGGER.warn(e);
+        }
+
+    }
+
+    private static SessionFactory createSessionFactory() {
+        Configuration configuration = new Configuration()
+                .addAnnotatedClass(Client.class)
+                .configure();
+
+        StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder()
+                .applySettings(configuration.getProperties());
+
+        return configuration.buildSessionFactory(builder.build());
+    }
+
+    private static void toSellCarSteve() {
         HashMap<Integer, Product> products = new HashMap<>();
         Product car = new Product(1, "Car", 300);
         products.put(1, car);
 
         Store store = new Store("Store 1", products);
-        Client steve = new Client("Steve", 300);
 
-        store.toSell(1,steve);
+        store.toSell(1, steve);
     }
 }
--- src/main/java/com/github/kamys/entity/Client.java
+++ src/main/java/com/github/kamys/entity/Client.java
@@ -1,5 +1,6 @@
 package com.github.kamys.entity;
 
+import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -7,18 +8,24 @@ import java.util.List;
  * Represents client which to buys {@link Product} of {@link Store}.
  * Contains list to bought{@link Product}.
  */
+@Entity
+@Table(schema = "public", name = "clients")
 public class Client {
     /**
      * Client name.
      */
+    @Id
+    @Column(name = "name")
     private String name;
     /**
      * Client balance for to buy {@link Product}.
      */
+    @Column(name = "balance")
     private int balance;
     /**
      * List to bought{@link Product}
      */
+    @Transient
     private List<Product> boughtProducts = new ArrayList<>();
 
     /**
